12dc48499bdb1b88d52bf6e09b9bf7fd8796180b
Aurelian Tutuianu <tutuianu@amazon.com>
2023-04-19 16:18:39 +0300

    - connection properties

---
+++ src/main/java/org/rapaio/jupyter/kernel/base/core/ConnectionProperties.java
@@ -0,0 +1,28 @@
+package org.rapaio.jupyter.kernel.base.core;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import org.rapaio.jupyter.kernel.base.message.HMACDigest;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+public record ConnectionProperties(
+        @SerializedName("control_port") int controlPort,
+        @SerializedName("shell_port") int shellPort,
+        @SerializedName("transport") String transport,
+        @SerializedName("signature_scheme") String signatureScheme,
+        @SerializedName("stdin_port") int stdinPort,
+        @SerializedName("hb_port") int hbPort,
+        @SerializedName("ip") String ip,
+        @SerializedName("iopub_port") int iopubPort,
+        @SerializedName("key") String key) {
+
+    public HMACDigest createHMACDigest() throws InvalidKeyException, NoSuchAlgorithmException {
+        if (key == null || key.isEmpty()) {
+            return HMACDigest.NO_AUTH_INSTANCE;
+        }
+        return new HMACDigest(signatureScheme, key);
+    }
+}
---
+++ src/test/java/org/rapaio/jupyter/base/core/ConnectionPropertiesTest.java
@@ -0,0 +1,49 @@
+package org.rapaio.jupyter.base.core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.jupiter.api.Test;
+import org.rapaio.jupyter.kernel.base.core.ConnectionProperties;
+import org.rapaio.jupyter.kernel.base.core.Transform;
+import org.rapaio.jupyter.kernel.base.message.HMACDigest;
+
+public class ConnectionPropertiesTest {
+
+    @Test
+    void testSerialization() throws NoSuchAlgorithmException, InvalidKeyException {
+
+        String text = """
+                {
+                  "control_port": 50160,
+                  "shell_port": 57503,
+                  "transport": "tcp",
+                  "signature_scheme": "hmac-sha256",
+                  "stdin_port": 52597,
+                  "hb_port": 42540,
+                  "ip": "127.0.0.1",
+                  "iopub_port": 40885,
+                  "key": "a0436f6c-1916-498b-8eb9-e81ab9368e84"
+                }
+                                """;
+
+        var kcp = Transform.fromJson(text, ConnectionProperties.class);
+        assertEquals(50160, kcp.controlPort());
+        assertEquals(57503, kcp.shellPort());
+        assertEquals("tcp", kcp.transport());
+
+        var hmacGen = kcp.createHMACDigest();
+
+        byte[][] buffers = new byte[][] {
+                "test1".getBytes(StandardCharsets.US_ASCII),
+                "test2".getBytes(StandardCharsets.US_ASCII)
+        };
+
+        String hmac = hmacGen.calculateSignature(buffers);
+        assertEquals(hmac, hmacGen.calculateSignature(buffers));
+        assertEquals("", HMACDigest.NO_AUTH_INSTANCE.calculateSignature(buffers));
+    }
+}
